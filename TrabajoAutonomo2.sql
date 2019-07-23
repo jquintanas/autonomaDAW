e53a82115c52c890606f0ca48bedac29a6863a6f
kbburgos <43821172+kbburgos@users.noreply.github.com>
2019-07-22 22:00:44 -0500

    ADD base de datos

---
+++ TrabajoAutonomo2.sql
@@ -0,0 +1,17 @@
+create database trabajoautonomo2;
+use trabajoautonomo2;
+
+create table persona(
+	nombre varchar(50),
+    apellidos varchar(50),
+    edad int,
+    fecha_nacimiento date
+    );
+    
+    
+insert into persona values('Karla', 'Burgos', 23, '1996-03-23');
+insert into persona values('Olga', 'Gayrey', 35, '1984-01-6');
+insert into persona values('Rodolfo', 'Cornejo', 82, '1937-02-21');
+insert into persona values('Carlos', 'Mendez', 47, '1972-07-07');
+
+select * from persona;
